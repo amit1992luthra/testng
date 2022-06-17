80f64b9289eef9a88af0905a1a26ca63abea7ac3
amit.luthra1992@gmail.com <amit.luthra1992@gmail.com>
2022-06-17 14:39:36 +0530

    day 1

--- src/test/java/IBM1/testngexample1/testngday1.java
+++ src/test/java/IBM1/testngexample1/testngday1.java
@@ -35,33 +35,19 @@ public class testngday1 {
 		driver.get("https://demo.guru99.com/test/newtours/register.php");
 	}
 	
-	
-	@Test(enabled = false)
-	public void testcase1()
-	{
-		System.out.println("test case 1");
-		driver.findElement(By.name("lastName")).sendKeys("last");
-		driver.findElement(By.name("userName")).sendKeys("amit@a.com");
-		driver.findElement(By.name("email")).sendKeys("abcd");
-		driver.findElement(By.name("password")).sendKeys("123456789");
-		driver.findElement(By.name("confirmPassword")).sendKeys("123456789");
-		String tag=	driver.findElement(By.xpath("//*[text()='Phone:']")).getTagName();
-		System.out.println(tag);
-		driver.findElement(By.name("submit")).click();
-	}
-	
-	@Test(enabled = true)
-	public void testcase2()
-	{
-		System.out.println("test case 2");
-	}
-	
-
 	@Test
-	public void testcase3()
+	public void zbookingtickets()
 	{
-		System.out.println("test case 3");
+		System.out.println("generating PNR Number");
 	}
+	
+	@Test(dependsOnMethods="zbookingtickets")
+	public void checkingPNRStatus()
+	{
+		System.out.println("gchecking pnr status");
+	}
+	
+	
 	
 	
 	@AfterMethod
